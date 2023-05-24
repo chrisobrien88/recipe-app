ca6b9641c50f635c17fb30af8274c71f593d70e7
Chris obrien <chrisobrien88@gmail.com>
2023-05-24 09:37:35 +0200

    introducing route prop and params

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,4 +1,5 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
+// import { useNavigation } from "@react-navigation/native";
 
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
@@ -7,10 +8,16 @@ import React from "react";
 import Category from "../models/category";
 
 const CategoriesScreen = ({ navigation }) => {
+  // you only get the navigation prop if the component is registered as a screen in the Stack.navigator in app.js
+  // or you can use the useNavigation hook
 
   const renderCategoryItem = (itemData) => {
     const pressHandler = () => {
-      navigation.navigate("MealsOverview");
+      // the first argument is the name of the screen you want to navigate to
+      // the second argument is the data you want to pass to the screen
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      });
     };
 
     return (
@@ -25,7 +32,12 @@ const CategoriesScreen = ({ navigation }) => {
   return (
     <FlatList
       data={CATEGORIES}
+      // the keyExtractor is used to extract the key from the data prop
       keyExtractor={(item) => item.id}
+      // renderItem is a function that returns a react component
+      // in renderItem, Flatlist will pass an object with the following properties: item, index, separators
+      // item is the item from the data prop using the id from the keyExtractor
+      // this is why the renderItem function does not need to accept any arguments, it is passed the itemData object (see above)
       renderItem={renderCategoryItem}
       numColumns={2}
     />
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,16 @@
 import { MEALS } from "../data/dummy-data";
 import { StyleSheet, Text, View } from "react-native";
+// import { useRoute } from "@react-navigation/native";
 
-const MealsOverviewScreen = (props) => {
+// parent component: CategoriesScreen
+const MealsOverviewScreen = ({ route }) => {
+// the route prop is passed to the component by the navigator
+// the route prop contains the params prop which contains the data passed to the screen
+// categoryId is passed in the parent component (CategoriesScreen) in the onPress handler
+    const catId = route.params.categoryId;
     return (
         <View style={styles.container}>
-            <Text>The Meals Overview Screen!</Text>
+            <Text>The Meals Overview Screen! {catId} </Text>
         </View>
     );
 };
